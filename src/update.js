cabb6b6f4ca5531a0b49fe368100aff72bc0cac4
Brody Dingel <brodydingel795@gmail.com>
2021-02-18 22:58:05 -0600

    Process emails

--- src/update.js
+++ src/update.js
@@ -1,15 +1,10 @@
 import {getEmailsAndMarkAsRead} from './get-emails.js';
-
-// import {processEmails} from './process-emails.js';
+import {processEmails} from './process-emails.js';
 
 export const update = async () => {
     const emails = await getEmailsAndMarkAsRead();
 
-    console.log('emails', emails);
-
-    /*
-     * if (emails.length) {
-     * 	processEmails(emails);
-     * }
-     */
+    if (emails.length) {
+        await processEmails(emails);
+    }
 };
