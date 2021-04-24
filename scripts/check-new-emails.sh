9a1de5ed18db6261ad9ef9e9d334e266d303c9d9
Brody Dingel <brodydingel795@gmail.com>
2021-04-24 15:00:07 -0500

    Update check emails script path

--- scripts/check-new-emails.sh
+++ scripts/check-new-emails.sh
@@ -1,8 +1,8 @@
 #!/bin/bash
 
 DIR="$( cd "$( dirname "${BASH_SOURCE[0]}" )" >/dev/null 2>&1 && pwd )"
-cd ${DIR}
+cd "${DIR}/.."
 source /root/.nvm/nvm.sh
 nvm use
 
-node ../index.js
+node ./src/update-transactions.js
